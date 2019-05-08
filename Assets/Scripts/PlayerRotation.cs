04d76fb1ade54031a1e3624c18e50df4c932cf57
Chandler <chosteler@mail.pima.edu>
2019-05-08 15:53:35 -0700

    Movement correct but duplication bug

---
+++ Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+    public float rotateSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotateSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/ShootSelf.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootSelf : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject shotPlayer;
+    public int speed;
+    bool isInstanciated;
+    public float coolDownTime;
+    private float nextFiretime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shoot();
+
+        if (!isInstanciated)
+        {
+            StartCoroutine(waitForInstanciate());
+            StopAllCoroutines();
+
+            isInstanciated = true;
+        }
+
+        fire();
+    }
+    public bool shoot()
+    {
+
+        bool bulletCreated = false;
+        Vector2 playerPos = player.transform.position;
+        Vector2 mousePos = Input.mousePosition;
+        Vector2 screenPos = Camera.main.ScreenToWorldPoint(new Vector2(mousePos.x, mousePos.y));
+        Quaternion q = Quaternion.FromToRotation(Vector2.up, screenPos - playerPos);
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (transform.localScale.x < 0 && bulletCreated == false && nextFiretime < Time.time)
+            {
+                nextFiretime = Time.time + coolDownTime;
+                player = Instantiate(shotPlayer, new Vector2(playerPos.x, playerPos.y), q);
+            }
+            else if (nextFiretime < Time.time)
+            {
+                nextFiretime = Time.time + coolDownTime;
+                player = Instantiate(shotPlayer, new Vector2(playerPos.x, playerPos.y), q);
+            }
+            bulletCreated = true;
+        }
+        return bulletCreated;
+    }
+
+
+
+
+
+
+    public void fire()
+    {
+        GetComponent<Rigidbody2D>().AddForce(GetComponent<Rigidbody2D>().transform.up * speed);
+    }
+
+    IEnumerator waitForInstanciate()
+    {
+
+        yield return new WaitUntil(() => shoot());
+
+    }
+}
